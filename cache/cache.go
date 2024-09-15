1c96f60b630aab0773d460d73dc230674a9f8a67
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2024-09-15 15:11:39 -0500

    ADD Generics to Cache

--- cache/cache.go
+++ cache/cache.go
@@ -4,11 +4,19 @@ import (
 	"sync"
 )
 
-type SafeCache struct {
-    mu sync.Mutex
-    v map[string]string
+type SafeCache[T any] struct {
+	mu    sync.Mutex
+	value map[string]T
 }
 
-func (sc *SafeCache) Set(key, value string) {
-    sc.mu.
+func (sc *SafeCache[T]) Set(key string, value T) {
+	sc.mu.Lock()
+	sc.value[key] = value
+	sc.mu.Unlock()
+}
+
+func (sc *SafeCache[T]) Get(key string) T {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+	return sc.value[key]
 }
